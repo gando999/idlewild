7e9217cc94e1df8533c6f152b6285e883481dec5
gando999 <gary.anderson@fanduel.com>
2017-11-17 16:54:23 +0000

    Start unit tests

---
+++ idlewild/tests/test_builder.py
@@ -0,0 +1,94 @@
+import pytest
+
+from graphql import (
+    GraphQLObjectType,
+    GraphQLField,
+    GraphQLString,
+)
+
+from idlewild.builder import Builder
+
+
+@pytest.fixture
+def plain_builder():
+    return Builder()
+
+
+@pytest.fixture
+def generic_graphql_field():
+    return GraphQLField(type=GraphQLString)
+
+
+@pytest.fixture
+def generic_graphql_object(generic_graphql_field):
+    return GraphQLObjectType(
+        'foo', fields={'bar': generic_graphql_field}
+    )
+
+
+@pytest.fixture
+def field_query_args():
+    return ('FIELD', 'query', 'ARGS', [])
+
+
+@pytest.fixture
+def field_atom_nullable_querytype():
+    return (
+        'GRAPHQL_ATOM', ('GRAPHQL_TYPE', 'NULLABLE', 'QueryType')
+    )
+
+
+@pytest.fixture
+def field_atom_nullable_stringtype():
+    return (
+        'GRAPHQL_ATOM', ('GRAPHQL_TYPE', 'NULLABLE', 'String')
+    )
+
+
+@pytest.fixture
+def schemadef_node(field_query_args, field_atom_nullable_querytype):
+    return (
+        'SCHEMADEF', 'schema',
+        [(field_query_args ,field_atom_nullable_querytype)]
+    )
+
+
+@pytest.fixture
+def type_node(field_query_args, field_atom_nullable_stringtype):
+    return (
+        'TYPE', 'Foo', 'Bar',
+        [('FooBar', field_atom_nullable_stringtype)]
+    )
+
+            
+def test_schemadef(
+        plain_builder, schemadef_node,
+        generic_graphql_object, generic_graphql_field):
+    
+    plain_builder.types = {'QueryType': generic_graphql_object}
+    plain_builder.build([schemadef_node])
+    
+    schema = plain_builder.schema
+    query_type = schema.get_query_type()
+    fields = query_type.fields
+    
+    assert fields['bar'] == generic_graphql_field
+
+
+def test_type(
+        plain_builder, type_node,
+        schemadef_node, generic_graphql_object,
+        field_atom_nullable_stringtype,
+        field_atom_nullable_querytype):
+    
+    plain_builder.types = {'QueryType': generic_graphql_object}
+    plain_builder.build([schemadef_node, type_node])
+
+    types = plain_builder.types
+    
+    assert 'Foo' in types
+    types['Foo'] == field_atom_nullable_stringtype
+
+    assert 'QueryType' in types
+    types['QueryType'] == field_atom_nullable_querytype
+    
